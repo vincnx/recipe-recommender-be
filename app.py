c43853f59463bbd3f91708c492f41f5f0de939f4
vincent exelcio <vincent.celcio@gmail.com>
2026-06-15 23:57:58 +0700

    feat: add uv for package manager

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
-from flask import Flask, request
-from src.recipe.v1.internal.adapter.repo import recipe_repo
-from src.recipe.v1.internal.adapter.handler import recipe_handler
-from src.auth.v1.internal.adapter.handler import auth_handler
-from src.user.v1.internal.adapter.handler import user_handler
-from src.container import Container
+import nltk
+from flask import Flask
 from flask_cors import CORS
+
+from src.auth.v1.internal.adapter.handler import auth_handler
+from src.container import Container
+from src.recipe.v1.internal.adapter.handler import recipe_handler
+from src.recipe.v1.internal.adapter.repo import recipe_repo
+from src.user.v1.internal.adapter.handler import user_handler
 from src.user.v1.internal.adapter.repo import user_repo
+
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    nltk.download("punkt_tab")
     # TODO: move this
-    app.secret_key = 'your_secret_key'
+    app.secret_key = "your_secret_key"
     app.config.update(
         SESSION_PERMANENT=False,
         SESSION_COOKIE_SECURE=True,
-        SESSION_COOKIE_SAMESITE='None',
+        SESSION_COOKIE_SAMESITE="None",
         SESSION_COOKIE_HTTPONLY=True,
-        SESSION_COOKIE_DOMAIN=None
+        SESSION_COOKIE_DOMAIN=None,
     )
 
     CORS(app, supports_credentials=True)
@@ -28,17 +32,18 @@ def create_app() -> Flask:
     container = Container()
     container.wire(
         modules=[
-            recipe_handler.__name__, 
-            auth_handler.__name__, 
-            user_handler.__name__, 
+            recipe_handler.__name__,
+            auth_handler.__name__,
+            user_handler.__name__,
             user_repo.__name__,
-            recipe_repo.__name__
+            recipe_repo.__name__,
         ]
-    ) 
+    )
 
     return app
+
 
 if __name__ == "__main__":
     app = create_app()
 
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
