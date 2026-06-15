b13e15bff7bf93ac865b2b724e06088d8eda3d27
vincent exelcio <vincent.celcio@gmail.com>
2026-06-16 00:24:44 +0700

    feat: add predownload models on init

--- app.py
+++ app.py
@@ -10,8 +10,38 @@ from src.user.v1.internal.adapter.handler import user_handler
 from src.user.v1.internal.adapter.repo import user_repo
 
 
+def init_models():
+    from pathlib import Path
+
+    import gdown
+
+    MODEL_PATH = Path("models/word2vec.model")
+    RECIPE_VECTORS = Path("models/recipe_vectors.pkl")
+    RECIPE_DF = Path("models/recipes_df.pkl")
+
+    if not RECIPE_VECTORS.exists():
+        gdown.download(
+            "https://drive.google.com/file/d/18MkDVQonAdYZ8f7jFN58XabBmZx2tE5b/view?usp=drive_link",
+            str(RECIPE_VECTORS),
+            quiet=False,
+        )
+    if not RECIPE_DF.exists():
+        gdown.download(
+            "https://drive.google.com/file/d/13oCmTJO_8JM-Ar1LlPteW87V7DDnQ1DU/view?usp=drive_link",
+            str(RECIPE_DF),
+            quiet=False,
+        )
+    if not MODEL_PATH.exists():
+        gdown.download(
+            "https://drive.google.com/file/d/1EH0CUhFyqIvsFLHqeIUufkuc_BNbx5aX/view?usp=drive_link",
+            str(MODEL_PATH),
+            quiet=False,
+        )
+
+
 def create_app() -> Flask:
     app = Flask(__name__)
+    init_models()
     nltk.download("punkt_tab")
     # TODO: move this
     app.secret_key = "your_secret_key"
